4dafed1120926a43376325ea5f41b84347be5d4d
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2023-12-16 01:48:21 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Pair with given sum in a sorted array/pair-with-given-sum-in-a-sorted-array.java
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+            
+
+class Solution{
+    
+  
+    int Countpair(int a[], int n, int sum)
+    {
+         // Complete the function
+        int lp=0;
+        int rp=n-1;
+        int count =0;
+        while(lp<rp){
+            if(a[lp]+a[rp]==sum){
+                count ++;
+                lp++;
+                
+            }
+            else if(a[lp]+a[rp]>sum){
+                rp--;
+            }
+            else{
+                lp++;
+            }
+        }
+        
+        if(count ==0){
+            return -1;
+        }
+        
+        return count;
+    }
+  
+    
+}
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    
+		    String line1 = br.readLine();
+		    String[] element = line1.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    
+		    int arr [] = new int[sizeOfArray];
+		    
+		    String line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    int K = Integer.parseInt(br.readLine());
+		    
+		    Solution obj = new Solution();
+		   
+		    int res = obj.Countpair(arr, sizeOfArray, K);
+		    
+		    System.out.println(res);
+		    
+		    
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
