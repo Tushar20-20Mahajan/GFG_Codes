7fd0a21033df7a99c8bb75d2948abfff31bf6891
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2023-12-15 20:37:40 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Move all zeroes to end of array/move-all-zeroes-to-end-of-array.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[n];
+            String[] inputLine = br.readLine().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().pushZerosToEnd(arr, n);
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+//User function Template for Java
+
+class Solution {
+    void pushZerosToEnd(int[] arr, int n) {
+        // code here
+        java.util.ArrayList<Integer> list = new java.util.ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(arr[i]==0){
+                continue;
+            }
+            else{
+                list.add(arr[i]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i]==0){
+                list.add(arr[i]);
+            
+            }
+            else{
+                continue;
+            }
+        }
+        for(int i=0;i<n;i++){
+            arr[i]=list.get(i);
+        }
+    }
+}
