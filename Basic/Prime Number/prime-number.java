dc622ee8e9e38f7c6761741bcb1cc0970571265e
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2023-12-15 18:34:09 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Prime Number/prime-number.java
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.isPrime(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution{
+    static int isPrime(int N){
+        // code here
+        java.math.BigInteger ans = java.math.BigInteger.valueOf(N);
+        if(ans.isProbablePrime(10)){
+            return 1;
+        }
+        return 0;
+    }
+}
