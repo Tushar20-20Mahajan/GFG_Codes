991e2f3eecf13b121bd536cb29e38ca2702da1ee
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2023-12-15 18:03:22 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Check if a string is Isogram or not/check-if-a-string-is-isogram-or-not.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+ {
+	public static void main (String[] args) throws Exception
+	 {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(br.readLine());
+        while(testCase-- > 0)
+        {
+            String data = br.readLine();
+            Solution obj = new Solution();
+            System.out.println(obj.isIsogram(data)?"1":"0");
+            
+        }
+	 }
+}
+// } Driver Code Ends
+
+
+
+
+
+class Solution
+{
+    //Function to check if a string is Isogram or not.
+    static boolean isIsogram(String data){
+        //Your code here
+        HashSet<Character> list = new HashSet<>();
+        int n= data.length();
+        for(int i=0;i<n;i++){
+            list.add(data.charAt(i));
+            
+        }
+        if(n==list.size()){
+            return true;
+        }
+        return false;
+    }
+}
