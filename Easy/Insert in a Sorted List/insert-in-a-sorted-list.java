f9701ab3a71ccd775bc3e1699831eb2a3b8b964b
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2023-12-16 01:35:00 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Insert in a Sorted List/insert-in-a-sorted-list.java
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+	
+    public static void main(String args[]) throws IOException { 
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t > 0){
+            int n = sc.nextInt();
+            
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            
+            int k = sc.nextInt();
+            
+            Solution g = new Solution();
+            head = g.sortedInsert(head,k);
+            
+            printList(head); 
+            t--;
+        }
+    } 
+} 
+
+// } Driver Code Ends
+
+
+
+
+/*class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }*/
+
+// sortedInsert method should return the head of the modified linked list.
+class Solution {
+    Node sortedInsert(Node head1, int key) {
+        // Add your code here.
+         ArrayList<Integer> list = new ArrayList<>();
+        Node temp=head1;
+        while(temp!=null){
+            list.add(temp.data);
+            temp=temp.next;
+        }
+        list.add(key);
+        Collections.sort(list);
+        
+        Node dummy=new Node(0);
+        Node current = dummy;
+        for(int i=0;i<list.size();i++){
+            current.next=new Node(list.get(i));
+            current=current.next;
+        }
+        return dummy.next;
+    }
+}
