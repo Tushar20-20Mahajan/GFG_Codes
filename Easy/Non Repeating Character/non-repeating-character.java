976d2dcdb387d17df1ac9b179937c78198a7bc0d
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2023-12-15 19:37:08 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Non Repeating Character/non-repeating-character.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Driverclass
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            String st = sc.next();
+            
+            char ans=new Solution().nonrepeatingCharacter(st);
+            
+            if(ans!='$')
+            System.out.println(ans);
+            else
+            System.out.println(-1);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to find the first non-repeating character in a string.
+    static char nonrepeatingCharacter(String S)
+    {
+        //Your code here
+        ArrayList<Character> list = new ArrayList<>();
+        for(int i=0;i<S.length();i++){
+            list.add(S.charAt(i));
+        }
+        for(int i =0;i< S.length();i++){
+            if(Collections.frequency(list,S.charAt(i))==1){
+                return list.get(i);
+            }
+        }
+        return '$';
+    }
+}
