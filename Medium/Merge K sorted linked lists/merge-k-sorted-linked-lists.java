36470e45eead96a7084e11de60e48634b1cc45a5
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2023-12-16 14:22:18 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Merge K sorted linked lists/merge-k-sorted-linked-lists.java
@@ -0,0 +1,111 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+
+class GfG
+{
+    public static void printList(Node node)
+    {
+        while(node != null)
+        {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+    
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {   
+            int N = sc.nextInt();
+            
+            Node []a = new Node[N];
+            
+            for(int i = 0; i < N; i++)
+            {
+                int n = sc.nextInt();
+                
+                Node head = new Node(sc.nextInt());
+                Node tail = head;
+                
+                for(int j=0; j<n-1; j++)
+                {
+                    tail.next = new Node(sc.nextInt());
+                    tail = tail.next;
+                }
+                
+                a[i] = head;
+            }
+            
+            Solution g = new Solution();
+             
+            Node res = g.mergeKList(a,N);
+            if(res!=null)
+                printList(res);
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+
+/*class Node
+{
+    int data;
+    Node next;
+    
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+*/
+
+// a is an array of Nodes of the heads of linked lists
+// and N is size of array a
+
+
+class Solution
+{
+    //Function to merge K sorted linked list.
+    Node mergeKList(Node[]arr,int K)
+    {
+        //Add your code here.
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i=0;i<K;i++){
+            Node temp = arr[i];
+            while(temp!=null){
+                list.add(temp.data);
+                temp=temp.next;
+            }
+        }
+        
+        Collections.sort(list);
+        
+        Node dummy= new Node(0);
+        Node current= dummy;
+        for(int i=0;i<list.size();i++){
+            current.next=new Node(list.get(i));
+            current=current.next;
+        }
+        return dummy.next;
+    }
+}
