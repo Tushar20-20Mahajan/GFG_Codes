c124f43e139214a49c9721a8cd6f741aa26c973c
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2023-12-16 13:13:12 +0530

    Updated Solution - GfG to GitHub

--- Medium/Maximum Index/maximum-index.java
+++ Medium/Maximum Index/maximum-index.java
@@ -2,27 +2,51 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
-    
-	public static void main (String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
-		while(t-->0){
-		    int n = Integer.parseInt(br.readLine().trim()); // size of array
-		    int arr[] = new int[n];
-		    String inputLine[] = br.readLine().trim().split(" ");
-		    for(int i=0; i<n; i++){
-		        arr[i] = Integer.parseInt(inputLine[i]); // input elements of array
-		    }
-		    
-		    Solution ob = new Solution();
-		    
-		    System.out.println(ob.maxIndexDiff(arr, n)); // print the result
-		}
-	}
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            int arr[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            System.out.println(new Solution().maxIndexDiff(arr, n));
+        }
+    }
 }
 // } Driver Code Ends
 
+
+
+
+// class Solution {
+
+//     int maxIndexDiff(int arr[], int n) {
+//         // code here
+//     }
+// }
+
+// class Solution {
+//     static int maxIndexDiff(int A[], int n) { 
+//         int maxDiff = 0;
+        
+//         for (int i = 0; i < n; i++) {
+//             for (int j = n - 1; j > i + maxDiff; j--) {
+//                 if (A[i] <= A[j]) {
+//                     maxDiff = Math.max(maxDiff, j - i);
+//                     break;
+//                 }
+//             }
+//         }
+        
+//         return maxDiff;
+//     }
+// }
 
 
 class Solution {
