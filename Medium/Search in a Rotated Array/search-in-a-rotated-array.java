f75b0e521159cf56f996aa069e23ce082039f0b1
Tushar Mahajan <95735086+Tushar20-20Mahajan@users.noreply.github.com>
2023-12-16 14:31:21 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Search in a Rotated Array/search-in-a-rotated-array.java
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+public class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        
+        int t = Integer.parseInt(in.readLine().trim());
+        while (t-- > 0)
+        {
+            int n = Integer.parseInt(in.readLine().trim());
+            int A[] = new int[n];
+            String s[]=in.readLine().trim().split(" ");
+            for (int i = 0;i < n;i++)
+            {
+                A[i] = Integer.parseInt(s[i]);
+            }
+            int key = Integer.parseInt(in.readLine().trim());
+            
+            out.println(new Solution().search(A, 0, n - 1, key));
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+
+
+// //User function Template for Java
+
+// class Solution
+// {
+//     int search(int A[], int l, int h, int key)
+//     {
+//         // l: The starting index
+//         // h: The ending index, you have to search the key in this range
+//         // Complete this function
+//         if(A.length==2){
+//             if(A[0]==key){
+//                 return 0;
+//             }
+//             else if(A[1]==key){
+//                 return 1;
+//             }
+//         }
+//         for(int i=l;i<h;i++){
+//             if(A[i]==key){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+
+class Solution {
+    int search(int A[], int l, int h, int key) {
+        while (l <= h) {
+            int mid = (l + h) / 2;
+
+            if (A[mid] == key) {
+                return mid;
+            }
+
+            // If the left half is sorted
+            if (A[l] <= A[mid]) {
+                if (key >= A[l] && key <= A[mid]) {
+                    h = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
+            // If the right half is sorted
+            else {
+                if (key >= A[mid] && key <= A[h]) {
+                    l = mid + 1;
+                } else {
+                    h = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
